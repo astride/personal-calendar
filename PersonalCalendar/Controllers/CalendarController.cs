97612bfde94287d9167f8e0a3965165240cb9db9
Astrid Eldhuset <astrid.eldhuset@gmail.com>
2017-05-23 21:55:25 +0200

    Added CalendarController

---
+++ PersonalCalendar/Controllers/CalendarController.cs
@@ -0,0 +1,20 @@
+﻿using PersonalCalendar.Services;
+using System.Web.Mvc;
+
+namespace PersonalCalendar.Calendar.Controllers
+{
+    public class CalendarController : Controller
+    {
+        private IPlanService _planService;
+
+        public CalendarController(IPlanService planService)
+        {
+            _planService = planService;
+        }
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
