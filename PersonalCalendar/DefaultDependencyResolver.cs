debaf91c2029ede18f9669600b14763de8e268ae
Astrid Eldhuset <astrid.eldhuset@gmail.com>
2017-05-23 22:12:50 +0200

    #DependencyInjection: Added DefaultDependencyResolver

---
+++ PersonalCalendar/DefaultDependencyResolver.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace PersonalCalendar
+{
+    public class DefaultDependencyResolver : IDependencyResolver
+    {
+        protected IServiceProvider _serviceProvider;
+
+        public DefaultDependencyResolver(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _serviceProvider.GetService(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _serviceProvider.GetServices(serviceType);
+        }
+    }
+}
