9c04849bbd53689bca6ed20a7cd360ee57f3180a
Astrid Eldhuset <astrid.eldhuset@gmail.com>
2017-05-23 21:44:40 +0200

    Added PlanDbContext and created database

---
+++ PersonalCalendar.DataModel/PlanDbContext.cs
@@ -0,0 +1,21 @@
+﻿using PersonalCalendar.Models.Calendar;
+using System.Data.Entity;
+
+namespace PersonalCalendar.Data
+{
+    public class PlanDbContext : DbContext
+    {
+        public DbSet<Plan> Plans { get; set; }
+        public DbSet<Date> Dates { get; set; }
+        public DbSet<Time> Times { get; set; }
+
+        public PlanDbContext()
+            : base("PlanDb")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
